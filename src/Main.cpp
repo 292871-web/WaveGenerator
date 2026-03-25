9e4525b5485e0832d01787e4722b52e1921d82d3
Paweł Gralak <292871@student.pwr.edu.pl>
2026-03-25 15:37:31 +0100

    quickfix

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,19 +15,16 @@ int main(int argc, char **argv) {
     app.add_option("-p,--program", program, "Which program u are choosing, avaible are: \n- o for generating an static obj \n- a for generating an animation")->required();
     app.add_option("-r,--resolution", resolution, "How acurate the animation will be");
     app.add_option("-s,--sizeBox", sizeBox, "How big is going to be the plot");
+    app.add_option("-t,--timeResolution", timeResolution, "How accurate the time between animation's frames will be");
+    app.add_option("-m,--timeMax", timeMax, "How long the animation will be");
     CLI11_PARSE(app, argc, argv);
-    
     if (program = 'o') {
-    WaveGenerator generator(resolution, 10.0f);
+    WaveGenerator generator(resolution, sizeBox);
     auto points = generator.generateCoordinates();
     
     ObjExporter exporter;
     exporter.save("../output/QuantumWave.obj", points, resolution);
     } else if (program = 'a') {
-    app.add_option("-t,--timeResolution", timeResolution, "How accurate the time between animation's frames will be");
-    app.add_option("-m,--timeMax", timeMax, "How long the animation will be");
-    CLI11_PARSE(app, argc, argv);
-    
     AdvWaveGenerator generator(resolution, sizeBox, timeMax, timeResolution);
     auto coordinates = generator.AdvGenerateCoordinates();
     
